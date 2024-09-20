121dc07ab565cf293961c48349b8272d9e735d6f
Joseph Olawale Bamidele <47855279+jolawale28@users.noreply.github.com>
2024-09-20 02:32:58 +0100

    Added Nav links

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,21 +22,36 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-800 flex flex-col justify-between`}
         style={{ height: '100vh' }}>
-        <header className="flex justify-between items-center border-b border-gray-700">
-          <div className="p-20">
-            <h1 className="text-7xl font-bold">React</h1>
-            <div>Frontend Practice Concepts, exercises and tutorials</div>
+        <header className="border-b border-gray-700">
+          <div className="flex justify-between items-center ">
+            <div className="px-20 pt-20">
+              <h1 className="text-7xl font-bold">React</h1>
+              <div>Frontend Practice Concepts, exercises and tutorials</div>
+            </div>
+
+            <div className="px-20 pt-20 flex">
+              <a href="https://github.com/jolawale28" className="mr-3 hover:scale-105 rounded-full" title="Joseph Olawale B.">
+                <img src="https://avatar.iran.liara.run/public/20" style={{ width: '50px', height: '50px' }} />
+              </a>
+              <a href="https://github.com/heebu" className="mr-3 hover:scale-105 rounded-full" title="Idris Heebu">
+                <img src="https://avatar.iran.liara.run/public/56" style={{ width: '50px', height: '50px' }} />
+              </a>
+
+            </div>
           </div>
 
-          <div className="p-20 flex">
-            <a href="https://github.com/jolawale28" className="mr-3 hover:scale-105 rounded-full" title="Joseph Olawale B.">
-              <img src="https://avatar.iran.liara.run/public/20" style={{ width: '50px', height: '50px' }} />
-            </a>
-            <a href="https://github.com/heebu" className="mr-3 hover:scale-105 rounded-full" title="Idris Heebu">
-              <img src="https://avatar.iran.liara.run/public/56" style={{ width: '50px', height: '50px' }} />
-            </a>
-
-          </div>
+          <nav className="px-20 mt-10 mb-20">
+            <ul className="list-none flex space-x-5">
+              <li className="">
+                <a className="px-5 pb-5 relative group inline-block" href="">
+                  Home
+                  <div className="h-1 bg-yellow-500 group-hover:w-full absolute bottom-0 left-0 w-0 transition-all duration-500"></div>
+                </a>
+                
+              </li>
+              <li><a className="" href="">Counter</a></li>
+            </ul>
+          </nav>
         </header>
         {children}
 
--- src/app/page.js
+++ src/app/page.js
@@ -18,8 +18,8 @@ export default function Home() {
         <>
 
             <section className="px-20 mt-20">
-                <div class="grid grid-cols-4 gap-4">
-                    <div class="rounded overflow-hidden group cursor-pointer">
+                <div className="grid grid-cols-4 gap-4">
+                    <div className="rounded overflow-hidden group cursor-pointer">
                         <div className="overflow-hidden h-40">
                             <img className="group-hover:scale-105 transition-transform duration-1000" src="https://images.unsplash.com/photo-1549488933-2392c609e512?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
                         </div>
