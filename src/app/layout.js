89e7bc0bbcca5e27afaa20bb129386623e531b41
Joseph Olawale Bamidele <47855279+jolawale28@users.noreply.github.com>
2024-09-23 19:05:58 +0100

    file cleanup (delete)

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
   return (
 
     <html lang="en">
-
+      
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white flex flex-col`}
         style={{ height: '100vh' }}>
 
