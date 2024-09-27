64474176e164dc277da3746e998e0b38e6d1f172
Heebu Prime <ayblazz@gmail.com>
2024-09-27 17:45:52 +0100

    Chat Screen

--- src/app/ChatView/page.jsx
+++ src/app/ChatView/page.jsx
@@ -21,7 +21,10 @@ export default function ChatView() {
 
                             return (
                                 <div key={index} className={'flex bg-transparent rounded shadow p-1.5'}
-                                     onClick={() => setPage(index)}>
+                                     onClick={() => {
+                                         setPage(index)
+                                         console.log(pageNumber);
+                                     }}>
                                     <div className={'p-2 rounded-full bg-blue-200 w-12 h-12'}></div>
                                     <div className={'p-2 contain-style: text-white items-center'}>
                                         <div className={'text-sm font-bold'}>Idris</div>
