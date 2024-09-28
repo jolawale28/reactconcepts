32671a9f131617654e5c4dd29fdc9b83763a2a1d
Heebu Prime <ayblazz@gmail.com>
2024-09-28 06:45:34 +0100

    Chat Screen

---
+++ src/app/ChatView/ChatComp/chat_listtile.jsx
@@ -0,0 +1,14 @@
+export default function ChatListTile({name, message, image, messageId}) {
+    return (
+        <div key={messageId} className={'flex bg-transparent rounded shadow p-1.5'}>
+            <div className={'p-2 rounded-full bg-blue-200 w-12 h-12'}>
+                <image>{image}</image>
+            </div>
+
+            <div className={'p-2 contain-style: text-white items-center'}>
+                <div className={'text-sm font-bold'}>{name}</div>
+                <div className={'text-sm'}>{message}</div>
+            </div>
+        </div>
+    )
+}
--- src/app/ChatView/page.jsx
+++ src/app/ChatView/page.jsx
@@ -1,9 +1,17 @@
 'use client'
 
 import {useState} from "react";
+import ChatListTile from "@/app/ChatView/ChatComp/chat_listtile";
 
 export default function ChatView() {
     const [pageNumber, setPageNumber] = useState(0);
+
+    const allNames = [
+        {'name': 'Idris Adedeji', 'profilePics': 'google.com', 'lastMessage': 'Hello Olaiya'},
+        {'name': 'Abass Abdullah', 'profilePics': 'google.com', 'lastMessage': 'Hello Idris'},
+        {'name': 'Olaiya Abdul-Malik', 'profilePics': 'google.com', 'lastMessage': 'Hello Abass'},
+        {'name': 'Tope Joseph', 'profilePics': 'google.com', 'lastMessage': 'Hello Adedeji'},
+    ];
 
     function setPage(pageNumber) {
         setPageNumber(pageNumber);
@@ -13,35 +21,23 @@ export default function ChatView() {
         <div className={'chat-view flex bg-blue-950 p-2'}>
             <div className={'flex flex-col basis-1/5 h-96 bg-blue-0 rounded'}>
                 <div>
-                    <input type={'text'} enterKeyHint={'search'}
-                           className={'bg-gray-500 rounded max-w-max m-3 text-blue-950'}/>
+                    <input type={'text'} enterKeyHint={'search'} placeholder={'Search...'}
+                           className={'bg-blue-300 rounded-xl max-w-max m-3 text-blue-950 p-1.5'}/>
                 </div>
                 <div className={'bg-blue-950 basis-full h-full m-3 overflow-y-scroll'}>
 
                     {
-                        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14].map((item, index) => {
-
-                            return (
-                                <div key={index} className={'flex bg-transparent rounded shadow p-1.5'}
-                                     onClick={() => {
-                                         setPage(index)
-                                         console.log(pageNumber);
-                                     }}>
-                                    <div className={'p-2 rounded-full bg-blue-200 w-12 h-12'}></div>
-                                    <div className={'p-2 contain-style: text-white items-center'}>
-                                        <div className={'text-sm font-bold'}>Idris</div>
-                                        <div className={'text-sm'}>Hello Div</div>
-                                    </div>
-                                </div>
-                            );
+                        allNames.map((item, index) => {
+                            return <div key={index}>
+                                <ChatListTile name={item.name} message={item.lastMessage} messageId={index}
+                                              image={item.profilePics}></ChatListTile>
+                            </div>
                         })
                     }
                 </div>
             </div>
-            pageNumber == 2?
             <div className={`flex flex-col basis-4/5 h-96 bg-blue-300 rounded`}>
-            </div>: <div className={`flex flex-col basis-4/5 h-96 bg-red-300 rounded`}>
-        </div>
+            </div>
         </div>
     )
 }
