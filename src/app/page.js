9178cdb4c104a6d74054d357e413c70532710eb6
Joseph Olawale Bamidele <47855279+jolawale28@users.noreply.github.com>
2024-09-30 17:26:17 +0100

    Finalised Grid View implementation
    - implemented grid and list view buttons
    - buttons click to switch View

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,8 @@ import { Tooltip } from "@nextui-org/react";
 
 export default function Home() {
 
+    const [gridView, setGridView] = useState(false)
+
     const [apps, setApps] = useState([])
 
     const tasksData = useData()
@@ -56,63 +58,76 @@ export default function Home() {
                     </div>
                     <input value={searchInput} onChange={(e) => setSearchInput(e.target.value)} type="text" className="border py-2 px-5 rounded basis-2/3" placeholder="Search..." aria-placeholder="Search..." />
                     <div className="basis-1/3 flex justify-end gap-3 items-center">
-                        <button className="border p-3 hover:bg-gray-100 rounded"><Icon icon="mdi:format-list-bulleted" /></button>
-                        <button className="border p-3 hover:bg-gray-100 rounded"><Icon icon="mdi:view-grid" /></button>
+                        <button onClick={() => setGridView(false)} className={`border p-3 ${(!gridView) ? 'bg-black text-white': 'hover:bg-gray-100'} rounded`}><Icon icon="mdi:format-list-bulleted" /></button>
+                        <button onClick={() => setGridView(true)} className={`border p-3 ${(gridView) ? 'bg-black text-white': 'hover:bg-gray-100'} rounded`}><Icon icon="mdi:view-grid" /></button>
                     </div>
                 </div>
             </div>
-            <section className="px-5 sm:px-10 md:px-10 lg:px-20 mt-5">
-                <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-2 grid-cols-1 gap-5 px-3">
 
-                    {
-                        filteredApps.map((ele, index) =>
-                            <ProjectCard
-                                title={ele.title}
-                                desc={ele.desc}
-                                author_image={ele.author_image}
-                                author_link={ele.author_link}
-                                dest_link={ele.dest_link}
-                                image={ele.image}
-                                key={index}
-                            />)
-                    }
+            {
+                gridView && (
+                    <section className="px-5 sm:px-10 md:px-10 lg:px-20 mt-5">
+                        <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-2 grid-cols-1 gap-5 px-3">
 
-                </div>
-            </section>
-
-            <section className="px-5 sm:px-10 md:px-10 lg:px-20 mt-5">
-                <div className="px-3">
-
-                    <Table removeWrapper aria-label="Example static collection table">
-                        <TableHeader>
-                            <TableColumn style={{ width: '10%' }}>AVATAR</TableColumn>
-                            <TableColumn>TITLE</TableColumn>
-                            <TableColumn>DESCRIPTION</TableColumn>
-                            <TableColumn>AUTHOR</TableColumn>
-                            <TableColumn>DATE ADDED</TableColumn>
-                        </TableHeader>
-                        <TableBody>
                             {
-                                filteredApps.map((ele) => (
-                                    <TableRow key="1">
-                                        <TableCell>
-                                            <img className="group-hover:scale-110 transition-transform duration-1000 object-cover h-14 w-14 rounded" src={ele.image} alt={'app_icon'} />
-                                        </TableCell>
-                                        <TableCell>{ele.title}</TableCell>
-                                        <TableCell>{ele.desc}</TableCell>
-                                        <TableCell>
-                                            <Tooltip content={ele.author_name}>
-                                                <img className="group-hover:scale-110 transition-transform duration-1000 object-cover h-10 w-10 rounded" src={ele.author_image} alt={'author_image'} />
-                                            </Tooltip>
-                                        </TableCell>
-                                        <TableCell>Active</TableCell>
-                                    </TableRow>
-                                ))
+                                filteredApps.map((ele, index) =>
+                                    <ProjectCard
+                                        title={ele.title}
+                                        desc={ele.desc}
+                                        author_image={ele.author_image}
+                                        author_link={ele.author_link}
+                                        dest_link={ele.dest_link}
+                                        image={ele.image}
+                                        key={index}
+                                    />)
                             }
-                        </TableBody>
-                    </Table>
-                </div>
-            </section>
+
+                        </div>
+                    </section>
+                )
+            }
+
+            {
+                !gridView && (
+                    <section className="px-5 sm:px-10 md:px-10 lg:px-20 mt-5">
+                        <div className="px-3">
+
+                            <Table removeWrapper aria-label="Example static collection table">
+                                <TableHeader>
+                                    <TableColumn style={{ width: '10%' }}>AVATAR</TableColumn>
+                                    <TableColumn>TITLE</TableColumn>
+                                    <TableColumn>DESCRIPTION</TableColumn>
+                                    <TableColumn>AUTHOR</TableColumn>
+                                </TableHeader>
+                                <TableBody>
+                                    {
+                                        filteredApps.map((ele) => (
+                                            <TableRow key="1">
+                                                <TableCell>
+                                                    <img className="group-hover:scale-110 transition-transform duration-1000 object-cover h-14 w-14 rounded" src={ele.image} alt={'app_icon'} />
+                                                </TableCell>
+                                                <TableCell>
+                                                    <Link className="underline-offset-4 underline" href={ele.dest_link}>
+                                                        {ele.title}
+                                                    </Link>
+                                                </TableCell>
+                                                <TableCell>{ele.desc}</TableCell>
+                                                <TableCell>
+                                                    <Tooltip content={ele.author_name}>
+                                                        <Link href={ele.author_link}>
+                                                            <img className="group-hover:scale-110 transition-transform duration-1000 object-cover h-10 w-10 rounded" src={ele.author_image} alt={'author_image'} />
+                                                        </Link>
+                                                    </Tooltip>
+                                                </TableCell>
+                                            </TableRow>
+                                        ))
+                                    }
+                                </TableBody>
+                            </Table>
+                        </div>
+                    </section>
+                )
+            }
         </>
     );
 }
